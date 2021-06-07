8c5cc8cbaaa7bb8170f5dd69fbf8e25caafb612a
manisha54 <manishachaudhary2057@gmail.com>
2021-06-07 09:14:30 +0545

    Initial commit

---
+++ One.py
@@ -0,0 +1,7 @@
+'''
+1.Write a program to find the area of triangle.
+'''
+height=float(input('enter the height of the triangle: '))
+base=float(input('enter the base of the triangle:' ))
+area=height*base/2
+print(f'The area of triangle is {area}')
---
+++ venv/Third.py
@@ -0,0 +1,6 @@
+'''
+3.Write a program to find the perimeter of circle.
+'''
+radius=float(input('Enter the radius of circle: '))
+perimeter=2*22/7*radius
+print(f'The perimeter of circle is {perimeter}')
