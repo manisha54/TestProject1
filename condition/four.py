d4f857380e4c5d853e01d5df683c8c6bd27d93b2
manisha54 <manishachaudhary2057@gmail.com>
2021-06-19 12:44:25 +0545

    kg or lbs

--- condition/four.py
+++ condition/four.py
@@ -4,12 +4,12 @@ weight in kg then convert it into lbs
 else convert it to kg.
 '''
 weight=int(input("enter your weight: "))
-unit = input('Is the weight of a person in kg or lbs?: ')
-kg=int(weight*2.2)
-lbs=int(weight/2.2)
-if unit==kg:
-    print(f"the weight in kilogram is: {lbs}")
-elif unit==lbs:
-    print(f'the weight in pound is: {kg}')
+unit = input('enter unit (kg/lbs): ').lower()
+if unit =='kg':
+    converted_weight = weight*2.20462
+    print(f"your weight is {converted_weight} lbs")
+elif unit == 'lbs':
+    converted_weight = weight*0.453592
+    print(f'your weight is {converted_weight} kg')
 else:
-    print("please provide the valid weight")
+    print("use either 'kg' or 'lbs' as unit.")
