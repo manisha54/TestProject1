2abb6754651e1ad5a76e9b001bd6732306d7c180
manisha54 <manishachaudhary2057@gmail.com>
2021-06-21 18:05:13 +0545

    odd or even

--- Labtwo/one.py
+++ Labtwo/one.py
@@ -2,7 +2,8 @@
 1. Check whether 5 is in list of first 5 natural numbers or not. Hint: List => [1,2,3,4,5]
 '''
 list=[1,2,3,4,5]
-if 5 in list:
-    print(f'5 is in list')
+given_number=5
+if given_number in range(1,6):
+    print(f'number is in the list')
 else:
-    print(f'5 is not in list')
+    print(f'number is not in the list')
--- Labtwo/two.py
+++ Labtwo/two.py
@@ -3,23 +3,21 @@
  percentage and grade. Hint: more than 70% –>
  distinction, more than 60% –> first, more than 40% –> pass, less than 40% –> fail
 '''
-sub1 = float(inpu("enter your marks in first subjec: "))
+sub1 = float(input("enter your marks in first subject: "))
 sub2 = float(input("enter your marks in second subject: "))
 sub3 = float(input("enter your marks in third subject: "))
-sub4 = float(input("enter your marks in fouth subject: "))
-total_marks = sub1 +sub2 + sub3 + sub4
-print(f"the total marks of four subject is {total_marks}"
-percentage = total/4
-print(f'yours percentage is {percentage}')
-
-if total_marks>70:
+sub4 = float(input("enter your marks in fourth subjec: "))
+total= sub1 +sub2 + sub3 + sub4
+print(f"the total marks  is {total}"
+percentage=total/4
+if percentage>70:
     grade="distinction"
-elif total_marks>60%:
+elif percentage>60:
     grade="first division"
-elif total_marks>4o:
+elif percentage>4o:
     grade="pass"
 else:
-    grade:"fail"
+    grade="fail"
 print(f'your grade is grade')
-
+print(f'your percentage is {percentage}')
 
