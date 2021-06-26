51485942e12cc7202894583b785f9e542becf496
manisha54 <manishachaudhary2057@gmail.com>
2021-06-26 10:28:07 +0545

    float

---
+++ Labtwo/fifteen.py
@@ -0,0 +1,17 @@
+'''
+15. What will be the output of the following if a =2, b=3, c =4, d = 3?
+'''
+a=int(2)
+b=int(3)
+c=int(4)
+d=int(3)
+print(a==b)
+print(a!=d)
+print(b==d)
+print(a!=c)
+print(a+c)
+print(b/d)
+print(b>a)
+print(a<d)
+print(b-a==c-b)
+print(b>=d)
