d5e3e013c2c2484b7d51d2ab1356b26669eb35a0
manisha54 <manishachaudhary2057@gmail.com>
2021-07-07 09:26:45 +0545

    multiple of all items in list

---
+++ list/two.py
@@ -0,0 +1,8 @@
+"""
+2.Write a Python program to multiplies all the items in a list.
+"""
+list = [1,2,3,4]
+product=1
+for i in list:
+    product=product*i
+print(f"the multiple of all the items is {product}")
