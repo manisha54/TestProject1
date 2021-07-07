c2698f73ceaf742653d2f6ad25d685298553a475
manisha54 <manishachaudhary2057@gmail.com>
2021-07-07 07:28:08 +0545

    row and column using nested loop

---
+++ Labfour/four.py
@@ -0,0 +1,22 @@
+"""
+4.Write a Python program to construct the following pattern,
+using a nested for loop.
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
+for i in range(5):
+    for j in range(i):
+        print("*",end=' ')
+    print()
+
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*",end=' ')
+    print()
